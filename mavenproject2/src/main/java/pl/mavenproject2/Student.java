0ff0e699dee2bcb53e5f5c9959436654927456e2
paweł <pawel.lewandowski.kj@gmail.com>
2019-01-18 20:47:10 +0100

    update

--- mavenproject2/src/main/java/pl/mavenproject2/Student.java
+++ mavenproject2/src/main/java/pl/mavenproject2/Student.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.mavenproject2;
 
 /**
  *
  * @author plewandowski
  */
-public interface Student {
+public abstract class Student {
     
+    public void printName(){
+        System.out.println(getName());
+    }
+    
+    abstract String getName();
 }
